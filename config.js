43aec3a476eca1751451fb11cfd83b594eefc561
Your Name <your.email@example.com>
2025-03-25 22:03:45 +0530

    Remove API key and add sample config file

--- config.js
+++
@@ -1,3 +0,0 @@
-const config = {
-    API_KEY: 'eafa9083dd09cadd522b911361584c49' // Replace with your new OpenWeatherMap API key
-}; 
---
+++ config.sample.js
@@ -0,0 +1,3 @@
+const config = {
+    API_KEY: 'YOUR_OPENWEATHER_API_KEY_HERE' // Get your API key from openweathermap.org
+}; 
