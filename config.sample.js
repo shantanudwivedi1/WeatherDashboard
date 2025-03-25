ab5ebbda6ccfce301c62e441c776963e0eeae08e
Your Name <your.email@example.com>
2025-03-25 15:47:06 +0530

    Remove config.sample.js from private repository

--- config.sample.js
+++
@@ -1,4 +0,0 @@
-// Rename this file to 'config.js' and add your OpenWeatherMap API key
-const config = {
-    API_KEY: 'YOUR_OPENWEATHERMAP_API_KEY' // Get yours at: https://openweathermap.org/api
-}; 
